e8f6db66ac39d9aafe6dfa96045abdb4d2399ca1
dylanHulin <dylanhulin.focus@gmail.com>
2020-10-26 11:52:03 +0100

    Changement visibilité attribut apiService dans classe UserRepository

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class UserRepository {
 
-    protected final ApiService apiService; // TODO: A utiliser
+    private final ApiService apiService; // TODO: A utiliser
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
